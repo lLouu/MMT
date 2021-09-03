c313c7e0eea39f89529b44252fc12300054d9b93
lLouu <44877109+lLouu@users.noreply.github.com>
2021-09-03 23:33:36 +0200

    Modular errors (#19)
    ## Modified file(s)
     - errors.py
    
    ## Changelog
     - Added index and MAX_ID_MODULE to allow modular modifications

--- Source/errors.py
+++ Source/errors.py
@@ -3,4 +3,13 @@ class Error:
         pass
 
 def error(file_id, id, weight):
+    file_id = file_id if 0 <= file_id <= MAX_ID_MODULE else -1
     pass
+
+## Has to be set with the installer
+ # Modular section
+MAX_ID_MODULE = 0
+index = {
+        -1 :    "unkown",
+        0  :    "main module"
+        }
