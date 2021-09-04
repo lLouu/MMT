ce7f5704bdb948b72d52a7aa43a6f7dfe5da2a84
lLouu <44877109+lLouu@users.noreply.github.com>
2021-09-04 16:33:45 +0200

    Conflict UI & debugging
    ## Modified file(s)
    - module_loaders.py
    - options.dat
    - modules/default_ui folder
    
    ## Change log
    - Added option_file option, which log the overwritter option file
    - Debugged loader for it to be able to load module anywhere in the env
    - Added the user choice for ui when conflict is detected
    - Added a function to overwritte the default ui
    - Added a default ui module

--- Source/module_loaders.py
+++ Source/module_loaders.py
@@ -1,8 +1,10 @@
-from os import path as p, remove
+from os import path as p
+from sys import path as pyPath
 from glob import glob
 from errors import error
 
 class Option:
+    option_file = "Source\\options.dat"
     autoup = False
     module_folder = ["Source\\modules"]
     show_warning = False
@@ -61,6 +63,7 @@ def read_opt(path):
             o = simplify(l)
             if len(o) >= 2 and o[1] != '':
                 if o[0] == "overide_file":
+                    opt.option_file = o[1]
                     over_opt = read_opt(o[1])
                     return over_opt
                 elif o[0] == "autoup":
@@ -84,7 +87,6 @@ def read_opt(path):
                     else:
                         opt.ask_for_default = True
                 elif o[0] == "default_ui":
-                    # need a validation
                     opt.default_ui = o[1]
                 else:
                     error(0, 5, 0, "unkown option : " + o[0])
@@ -98,7 +100,7 @@ def getModules(opt = Option()):
     list = {"path": [], 
             "name": []}
     for path in opt.module_folder:
-        list["path"] += glob(path + "\*")
+        list["path"] += glob(path + "\\*")
     for i in range(len(list["path"])):
         if p.isfile(list["path"][i]):
             error(0, 7, 0, list["path"][i] + " is not a module")
@@ -108,17 +110,19 @@ def getModules(opt = Option()):
     return list
 
 def launch_ui(opt, list):
+    loaded = False
     if opt.default_ui != None:
         if opt.default_ui in list["name"]:
             path = list["path"][list["name"].index(opt.default_ui)]
             content = getContent(path + "\\lib.info").split('\n')
             if "main_ui" in content:
-                loader(path + "\\" + opt.default_ui + ".py", "main_ui")()
+                loader(opt.default_ui, "main_ui", path)()
+                loaded = True
             else:
                 error(0, 8, 1, opt.default_ui + " is not a ui module")
         else:
             error(0, 9, 1, opt.default_ui + " is not installed")
-    else:
+    if not loaded:
         ui_modules = {"name": [],
                       "path": []}
         for i in range(len(list["name"])):
@@ -128,22 +132,57 @@ def launch_ui(opt, list):
                 ui_modules["name"].append(list["name"][i])
         l = len(ui_modules["path"])
         if l == 1 or (l > 1 and not opt.ask_for_default):
-            loader(ui_modules["path"][0] + "\\" + ui_modules["name"][0] + ".py", "main_ui")
-        elif l == 0:
-            return False
-        else:
-            loader(ui_modules["path"][0] + "\\" + ui_modules["name"][0] + ".py", "main_ui")
-            ###################################
-            # Chooser to be done
-            ###################################
+            loader(ui_modules["name"][0], "main_ui", ui_modules["path"][0])()
+            loaded = True
+        elif l != 0:
+            index = 0
+            print("Multiple ui modules have been founded, which one do you want to use :\n")
+            for i in range(len(ui_modules["name"])):
+                print("[" + str(i+1) + "] - " + ui_modules["name"][i] + "\n")
+            index = int(input()) - 1
+            if not 0 <= index < len(ui_modules["name"]):
+                print("\nInvalid Input, taking the first UI module as default")
+                index = 0
+            print("\n\nDo you want to make" + ui_modules["name"][index] + "your default ui ? (Y/N)")
+            answer = input()
+            if(answer == "Y" or answer == 'y'):
+                setDefaultUI(opt, ui_modules["name"][index])
+            loader(ui_modules["name"][index], "main_ui", ui_modules["path"][index])()
+            loaded = True
 
+    return loaded
+
+def setDefaultUI(opt, ui_module):
+    content = getContent(opt.option_file).split('\n')
+    if content == None:
+        return False
+    finded = False
+    for i in range(len(content)):
+        buffer = 0
+        while buffer < len(content[i]) and content[i][buffer] == ' ':
+            buffer += 1
+        if len(content[i]) >= 10 + buffer:
+            if content[i][buffer:buffer+10] == "default_ui":
+                finded = True
+                content[i] = "default_ui=" + ui_module
+    if not finded:
+        content.append("default_ui=" + ui_module)
+    
+    content = "\n".join(content)
+    file = open(opt.option_file, "w")
+    file.write(content)
+    file.close()
     return True
+
+
 
 # from package import name as loader(package, name)
 # loader(package, name)(<vars>) will execute name with <vars>
 # a = loader(package, name) will allow a(<vars>) to execute name with <vars>
 # why don't we learn that and have to find this... beautiful thing by ourselves ;--;
-def loader(package, name):
+def loader(package, name, path = ''):
+    if path != '':
+        pyPath.append(path)
     return getattr(__import__(package, fromlist=[name]), name)
 
 
